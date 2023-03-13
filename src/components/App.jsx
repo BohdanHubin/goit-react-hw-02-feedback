9c3664f53ad8fa70bb12c851a1d50b0cf38fe783
BohdanHubin <111269462+BohdanHubin@users.noreply.github.com>
2023-03-13 20:25:20 +0200

    add statistic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Section from './Section/Section';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Statistics from './Statistics/Statistics';
 
 class App extends Component {
 state = {
@@ -28,13 +29,25 @@ state = {
 
 
   render() {
+    const { good, neutral, bad } = this.state;
     return (
     <div>
-        <Section title="Please leave feedback"></Section>
-        <FeedbackOptions
+        <Section title="Please leave feedback">
+          <FeedbackOptions
             options={Object.keys(this.state)}
             onLeaveFeedback={this.onLeaveFeedback}
           />
+        </Section>
+        <Section title="Statistics">
+          <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+        </Section>
+        
     </div>
   );}
 }
