365c48ced2089750de9d8d981e5d08de54edadd8
BohdanHubin <111269462+BohdanHubin@users.noreply.github.com>
2023-03-13 20:08:52 +0200

    add button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,42 @@
+import React, { Component } from 'react';
+
 import Section from './Section/Section';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+
+class App extends Component {
+state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = state => {
+    this.setState(prevState => ({
+      [state]: prevState[state] + 1,
+    }));
+  };
+
+  countTotalFeedback() {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  }
+
+  countPositiveFeedbackPercentage() {
+    const { good } = this.state;
+    return Math.round((good / this.countTotalFeedback()) * 100);
+  }
 
 
-export const App = () => {
-  return (
+  render() {
+    return (
     <div>
-      <Section title="Please leave feedback"></Section>
+        <Section title="Please leave feedback"></Section>
+        <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.onLeaveFeedback}
+          />
     </div>
-  );
-};
+  );}
+}
+
+export default App;
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,7 +1,7 @@
 import style from './Section.module.css';
 import PropTypes from 'prop-types';
 
-const Section = ({ title, children }) => {
+export default function Section ({ title, children }) {
   return (
     <section className={style.section}>
       <h2 className={style.title}>{title}</h2>
@@ -14,5 +14,3 @@ Section.propTypes = {
   title: PropTypes.string.isRequired,
   children: PropTypes.element,
 };
-
-export default Section;
